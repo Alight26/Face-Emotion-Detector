51603f7059877f5ce974a439bbbf15a7fb0e7d29
Lucas <luc.coulson6@gmail.com>
2025-09-07 01:44:19 -0400

    Emotions are now being detected using deepface.analyze, but it is not 100% accurate

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import cv2 as cv 
 from deepface import DeepFace 
 
-emotion = cv.CascadeClassifier('haarcascade_frontalFace_default.xml')
+detector = cv.CascadeClassifier('haarcascade_frontalFace_default.xml')
 
 cap = cv.VideoCapture(1)
 
 while True:
     ret, img = cap.read()
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    faces = emotion.detectMultiScale(gray, 1.3, 5)
+    faces = detector.detectMultiScale(gray, 1.3, 3)
 
     for (x, y, w, h) in faces:
+        
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+
+        # Does the Facial Analysis 
+        detection = DeepFace.analyze(roi_color, actions=['emotion'], enforce_detection=False)
+
+        # detects the most dominant emotion 
+        emotion = detection[0]['dominant_emotion']
+        
         cv.rectangle(img, (x,y), (x+w, y+h), (0, 0, 225), 2)
-        roi_gray = gray[x:x+w, y:y+h]
-        roi_color = img[x:x+w, y:y+h]
+        cv.putText(img, emotion, (50, 50), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1, cv.LINE_AA)
+
+
+
 
 
     cv.imshow('img', img)
